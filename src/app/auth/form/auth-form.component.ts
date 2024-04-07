187c21d9d3a66ad55da09d87e041bcec17b8469c
Akira <95500929+AkiraVerXlll@users.noreply.github.com>
2024-04-08 00:20:53 +0300

    add auth form

--- src/app/auth/form/auth-form.component.ts
+++ src/app/auth/form/auth-form.component.ts
@@ -1,14 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'auth-form',
     standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [RouterOutlet, NgOptimizedImage, NgIf],
     templateUrl: './auth-form.component.html',
     styleUrl: './auth-form.component.scss'
     })
 export class AuthFormComponent {
+  isSignIn: boolean = true;
 
+  toggleSignIn = () => {
+    this.isSignIn = !this.isSignIn;
+  }
 }
